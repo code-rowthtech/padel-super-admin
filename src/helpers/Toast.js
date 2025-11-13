7f81cc6a2b4d320a2b417ba2398a1c30bdbd0eec
Your Name <you@example.com>
2025-11-13 10:25:24 +0530

    mobile responsive

--- src/helpers/Toast.js
+++ src/helpers/Toast.js
@@ -1,19 +1,11 @@
 // src/utils/toast.js
-<<<<<<< HEAD
-import { toast, Slide } from "react-toastify";
-=======
 import { toast, Bounce } from "react-toastify";
->>>>>>> da00ece597a665ac1b9b9d5c398c194693ba1533
 
 export const showSuccess = (message = "Success!") => {
   toast.success(message, {
     position: "top-right",
     autoClose: 3000,
-<<<<<<< HEAD
-    transition: Slide,
-=======
     transition: Bounce,
->>>>>>> da00ece597a665ac1b9b9d5c398c194693ba1533
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
@@ -24,11 +16,7 @@ export const showError = (message = "Something went wrong") => {
   toast.error(message, {
     position: "top-right",
     autoClose: 4000,
-<<<<<<< HEAD
-    transition: Slide,
-=======
     transition: Bounce,
->>>>>>> da00ece597a665ac1b9b9d5c398c194693ba1533
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
@@ -39,11 +27,7 @@ export const showInfo = (message) => {
   toast.info(message, {
     position: "top-right",
     autoClose: 3000,
-<<<<<<< HEAD
-    transition: Slide,
-=======
     transition: Bounce,
->>>>>>> da00ece597a665ac1b9b9d5c398c194693ba1533
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
@@ -54,11 +38,7 @@ export const showWarning = (message) => {
   toast.warning(message, {
     position: "top-right",
     autoClose: 3000,
-<<<<<<< HEAD
-    transition: Slide,
-=======
     transition: Bounce,
->>>>>>> da00ece597a665ac1b9b9d5c398c194693ba1533
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
