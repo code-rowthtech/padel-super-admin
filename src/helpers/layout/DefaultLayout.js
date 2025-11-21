c354740843b0140243b617ea708645ae08fc03b6
Your Name <you@example.com>
2025-11-21 20:25:54 +0530

    ui issue

--- src/helpers/layout/DefaultLayout.js
+++ src/helpers/layout/DefaultLayout.js
@@ -65,8 +65,8 @@ const DefaultLayout = () => {
       {!shouldHideHeaderFooter && <Navbar user={user} />}
 
       <div
-        className="flex-grow-1"
-        style={{ marginTop: !shouldHideHeaderFooter ? "4rem" : "" }}
+        className="flex-grow-1 add_top_margin"
+        style={{ marginTop: !shouldHideHeaderFooter ? "" : "" }}
       >
         <Outlet />
       </div>
