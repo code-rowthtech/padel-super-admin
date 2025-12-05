4aa0ad86cc46563835e735e4fe22a8dfaacb7c1c
Your Name <you@example.com>
2025-12-05 13:23:45 +0530

    ui issue

--- src/pages/user/openMatches/MatchPlayer.js
+++ src/pages/user/openMatches/MatchPlayer.js
@@ -411,7 +411,7 @@ const MatchPlayer = ({
                     className="rounded-4 border row mx-auto pt-2 pb-0 mb-2"
                     style={{ backgroundColor: "#CBD6FF1A" }}
                 >
-                    <div className="d-flex d-block justify-content-between align-items-start py-2 border-bottom">
+                    <div className="d-flex d-block justify-content-between align-items-start py-2 border-bottom px-md-4 px-2">
                         <div className="d-flex align-items-center justify-content-md-between justify-content-start gap-2">
                             <img src={padal} alt="padel" width={24} />
                             <span className="ms-2 all-matches" style={{ color: "#374151" }}>
@@ -437,7 +437,7 @@ const MatchPlayer = ({
                     </div>
 
                     <div className="col-12 ps-0 text-center d-flex">
-                        <div className="col-md-4 col-5 py-2">
+                        <div className="col-md-5 col-5 py-2">
                             <p className="mb-1 add_font_mobile" style={{ fontSize: "13px", fontWeight: '500', fontFamily: "Poppins", color: "#374151" }}>
                                 Game Type
                             </p>
@@ -486,7 +486,7 @@ const MatchPlayer = ({
                             </p>
                         </div>
 
-                        <div className="col-md-4 col-3 py-2">
+                        <div className="col-md-3 col-3 py-2">
                             <p className="mb-1 add_font_mobile" style={{ fontSize: "13px", fontWeight: '500', fontFamily: "Poppins", color: "#374151" }}>
                                 Your share
                             </p>
