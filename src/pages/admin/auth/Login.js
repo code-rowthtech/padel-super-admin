05d36a7d83973325630ceb7146970edef508410d
pankaj sinhg <pankaj@gmail.com>
2026-01-09 17:12:57 +0530

    changes

--- src/pages/admin/auth/Login.js
+++ src/pages/admin/auth/Login.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FaEnvelope, FaEye, FaEyeSlash } from "react-icons/fa";
