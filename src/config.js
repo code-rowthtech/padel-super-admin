464f3a8c7b1bef913c61d2661be55ecf576d9540
Shubham <chandelshubham1604@gmail.com>
2026-03-18 10:31:24 +0530

    all filter in peayments

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // API_URL: process.env.REACT_APP_API_URL || 'http://103.142.118.40:7600',
-  API_URL: 'http://192.168.0.126:7600',
+  API_URL: process.env.REACT_APP_API_URL || 'http://103.142.118.40:7600',
+  // API_URL: 'http://192.168.0.126:7600',
   RAZORPAY_KEY: process.env.REACT_APP_RAZORPAY_KEY
 };
 export default config;
